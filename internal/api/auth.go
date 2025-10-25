7a76421528c1ca135c2ceba5e026b9453b8eab7f
Your Name <you@example.com>
2025-10-25 15:55:10 +0300

    Little changes

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -2,7 +2,7 @@ package api
 
 func ValidateToken(token string) bool {
 	client := NewSourceCraftClient(token)
-	body, err := client.DoRequest("GET", "/me/issues")
+	body, err := client.DoRequest("GET", "/me/issues", nil)
 
 	return body != nil && err == nil
 }
